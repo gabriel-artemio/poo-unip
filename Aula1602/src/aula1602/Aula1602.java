87cc721f99641e858cbace5041af1649976eeb47
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2023-02-17 01:32:52 +0100

    up

--- Aula1602/src/aula1602/Aula1602.java
+++ Aula1602/src/aula1602/Aula1602.java
@@ -1,7 +1,8 @@
 package aula1602;
+import javax.swing.JOptionPane;
 
 public class Aula1602 {
     public static void main(String[] args) {
-        System.out.print("Ola mundo");
+        JOptionPane.showMessageDialog(null,"Vai Corinthians");
     }
 }
---
+++ Aula1602/src/aula1602/MinhaPrimeiraClasse.java
@@ -0,0 +1,10 @@
+package aula1602;
+
+import javax.swing.JOptionPane;
+
+public class MinhaPrimeiraClasse {
+    public static void main(String[] args) {
+        //System.out.println("Vai Corinthians");
+        JOptionPane.showMessageDialog(null,"Hello");
+    }
+}
