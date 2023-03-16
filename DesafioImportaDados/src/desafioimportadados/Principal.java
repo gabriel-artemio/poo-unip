4292858d76fe6cbd544aede45adf007cf0ba2508
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2023-03-16 21:40:55 +0100

    add importa dados do desafio

---
+++ DesafioImportaDados/src/desafioimportadados/Principal.java
@@ -0,0 +1,10 @@
+package desafioimportadados;
+import interfaceProjeto.interfacePrincipal;
+
+public class Principal {
+    public static void main(String[] args) {
+        interfacePrincipal app = new interfacePrincipal();
+        app.setTitle("Importar dados Java");
+        app.setVisible(true);
+    }
+}
